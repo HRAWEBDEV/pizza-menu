e390e45ebc8ed798cdce075ba696c6ac562f0bea
hamidwise1 <hrawebdev@gmail.com>
2023-08-22 16:36:14 +0430

    add menu + add footer + add header components

--- src/index.js
+++ src/index.js
@@ -4,22 +4,55 @@ import { pizzaData } from './data';
 
 import './index.css';
 
-const Pizza = () => {
+const Header = () => {
  return (
-  <div>
-   <img src='pizzas/spinaci.jpg' alt='Pizza spinaci' />
-   <h2>pizza Spinaci</h2>
-   <p>Tomato, mozarella, spanish, and ricotta cheese</p>
+  <header className='header'>
+   <h1>Fast React pizza Co.</h1>
+  </header>
+ );
+};
+
+const Pizza = ({ name, ingredients, photoName, price }) => {
+ return (
+  <div className='pizza'>
+   <img src={photoName} alt={name} />
+   <div>
+    <h3>{name}</h3>
+    <p>{ingredients}</p>
+    <span>{price}</span>
+   </div>
   </div>
+ );
+};
+
+const Menu = () => {
+ return (
+  <main className='menu'>
+   <h2>our menu</h2>
+   <div className='pizzas'>
+    {pizzaData.map(({ name, ...other }) => (
+     <Pizza key={name} name={name} {...other} />
+    ))}
+   </div>
+  </main>
+ );
+};
+
+const Footer = () => {
+ return (
+  <footer className='footer'>
+   <p>{new Date().toLocaleTimeString()} We'r currently open</p>
+  </footer>
  );
 };
 
 const App = () => {
  return (
-  <>
-   <h1>react app template</h1>
-   <Pizza />
-  </>
+  <div className='container'>
+   <Header />
+   <Menu />
+   <Footer />
+  </div>
  );
 };
 
